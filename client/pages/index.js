5a92238e91ac86403b770b24251947b72b44960b
Andrew Yang <andy.tubeee@gmail.com>
2021-08-27 18:50:40 -0400

    Changed index page according to design

--- client/pages/index.js
+++ client/pages/index.js
@@ -12,25 +12,29 @@ export default function Home() {
         <title>Memelet</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className='text-6xl text-white px-10'>
+      <div className='text-8xl leading-snug text-white px-10'>
         the meme has <br />{' '}
         <span className='text-yellow-300 font-bold'>everything</span> that you
-        need
+        <br />{' '}
+        <div className='flex items-center'>
+          need
+          <Button
+            className='ml-16'
+            color='lightBlue'
+            buttonType='filled'
+            size='lg'
+            rounded={false}
+            block={false}
+            iconOnly={false}
+            ripple='light'
+            onClick={() => router.push('/generate')}
+          >
+            Create your Meme Flashcards
+            <Icon name='arrow_forward_ios' size='sm' />
+          </Button>
+        </div>
       </div>
       <br></br>
-      <Button
-        color='lightBlue'
-        buttonType='filled'
-        size='regular'
-        rounded={false}
-        block={false}
-        iconOnly={false}
-        ripple='light'
-        onClick={() => router.push('/generate')}
-      >
-        Create your Meme Flashcards
-        <Icon name='arrow_forward_ios' size='sm' />
-      </Button>
     </div>
   );
 }
