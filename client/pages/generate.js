65a77ec8018a7c25405eeda99f5b8b6c1129828a
Andrew Yang <andy.tubeee@gmail.com>
2021-08-27 19:15:45 -0400

    Added favicon

--- client/pages/generate.js
+++ client/pages/generate.js
@@ -3,12 +3,17 @@ import Icon from '@material-tailwind/react/Icon';
 import { useRouter } from 'next/router';
 import Input from '@material-tailwind/react/Input';
 import { useState } from 'react';
+import Head from 'next/head';
 
 const generate = () => {
   const router = useRouter();
   const [editHidden, setEditHidden] = useState(true);
   return (
     <div className='bg-[#FFE501] w-screen h-screen flex justify-center items-center flex-col gap-20'>
+      <Head>
+        <title>Memelet</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <div className='w-10/12 flex flex-col px-5 '>
         <input
           placeholder='Enter your question'
