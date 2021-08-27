920f1dcbb932bd378926f6bf183818cc1a266dd1
Andrew Yang <andy.tubeee@gmail.com>
2021-08-27 18:42:54 -0400

    Generate page with edit button

--- client/pages/generate.js
+++ client/pages/generate.js
@@ -1,31 +1,49 @@
 import Button from '@material-tailwind/react/Button';
 import Icon from '@material-tailwind/react/Icon';
+import { useRouter } from 'next/router';
+import Input from '@material-tailwind/react/Input';
+import { useState } from 'react';
 
 const generate = () => {
+  const router = useRouter();
+  const [editHidden, setEditHidden] = useState(true);
   return (
     <div className='bg-[#FFE501] w-screen h-screen flex justify-center items-center flex-col gap-20'>
-      <div className='w-10/12 flex flex-col items-center gap-6'>
+      <div className='w-10/12 flex flex-col px-5 '>
         <input
           placeholder='Enter your question'
-          className='px-5 py-3 rounded-lg text-purple-500 w-10/12'
+          className='px-5 py-3 my-4 rounded-lg text-purple-600 outline-none placeholder-purple-600'
         />
-        <input
-          placeholder='Type your own answer (optional)'
-          className='px-5 py-3 rounded-lg text-purple-500 w-10/12'
-        />
+        <div className='relative flex justify-center  rounded-lg  items-center bg-white'>
+          <input
+            placeholder='Type your own answer (optional)'
+            className='px-5 py-3 mb-4text-purple-600  rounded-lg outline-none placeholder-purple-600 flex-grow'
+          />
+          <Button
+            rounded
+            iconOnly
+            buttonType='outline'
+            color='deepPurple'
+            className={
+              'cursor-pointer border-0 mr-4 ' + `${editHidden ? 'hidden' : ''}`
+            }
+          >
+            <Icon name='edit' size='2xl' />
+          </Button>
+        </div>
       </div>
       <Button
-        color='lightBlue'
+        color='deepPurple'
         buttonType='filled'
         size='regular'
         rounded={false}
         block={false}
         iconOnly={false}
         ripple='light'
-        onClick={() => router.push('/generate')}
+        onClick={() => router.push('/')}
       >
         Generate
-        <Icon name='arrow_forward_ios' size='sm' />
+        <Icon name='double_arrow' size='sm' />
       </Button>
     </div>
   );
