690a1f03e0ef8bc431af7390cd644a0d8972d00a
Callum Irving <callum.irving04@gmail.com>
2021-08-28 15:00:33 -0400

    Added communication between client and our API

--- client/pages/generate.js
+++ client/pages/generate.js
@@ -8,14 +8,32 @@ import Head from 'next/head';
 const generate = () => {
 	const router = useRouter();
 	const [editHidden, setEditHidden] = useState(true);
+	const [question, setQuestion] = useState('');
+	const [answer, setAnswer] = useState('');
 
 	const clickHandler = async () => {
-		// const response = await fetch()
-		// const data = await response.json()
-		// const url = data.url
+		// Get image URL from our API
+		const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				question: question,
+				answered: answer !== '',
+				answer: answer !== '' ? answer : undefined,
+			}),
+		});
+		const data = await response.json();
+		const url = data.url;
 
 		// send url to result page
-		router.push('/result');
+		router.push({
+			pathname: '/result',
+			query: {
+				url: url,
+			},
+		});
 	};
 
 	return (
@@ -23,11 +41,19 @@ const generate = () => {
 			<div className="w-10/12 flex flex-col px-5 ">
 				<input
 					placeholder="Enter your question"
+					value={question}
+					onChange={(e) => {
+						setQuestion(e.currentTarget.value);
+					}}
 					className="px-5 py-3 my-4 rounded-lg text-purple-600 outline-none placeholder-purple-600"
 				/>
 				<div className="relative flex justify-center  rounded-lg  items-center bg-white">
 					<input
 						placeholder="Type your own answer (optional)"
+						value={answer}
+						onChange={(e) => {
+							setAnswer(e.currentTarget.value);
+						}}
 						className="px-5 py-3 text-purple-600  rounded-lg outline-none placeholder-purple-600 flex-grow"
 					/>
 					<Button
--- client/pages/result.js
+++ client/pages/result.js
@@ -5,27 +5,30 @@ import Input from '@material-tailwind/react/Input';
 import { useState } from 'react';
 
 const generate = () => {
-  const router = useRouter();
-  const [editHidden, setEditHidden] = useState(true);
-  return (
-    <div className='bg-[#FFE501] w-screen h-screen flex justify-center items-center flex-col gap-20'>
-      <div className='min-w-xl min-h-xl w-10/12 h-10 bg-white p-5 rounded-lg'></div>
-      <Button
-        className='group'
-        color='deepPurple'
-        buttonType='filled'
-        size='lg'
-        rounded={false}
-        block={false}
-        iconOnly={false}
-        ripple='light'
-        onClick={() => router.push('/')}
-      >
-        Download
-        <Icon name='file_download' size='sm' />
-      </Button>
-    </div>
-  );
+	const router = useRouter();
+	const [editHidden, setEditHidden] = useState(true);
+
+	// The image URL can be accessed by router.query.url
+
+	return (
+		<div className="bg-[#FFE501] w-screen h-screen flex justify-center items-center flex-col gap-20">
+			<div className="min-w-xl min-h-xl w-10/12 h-10 bg-white p-5 rounded-lg"></div>
+			<Button
+				className="group"
+				color="deepPurple"
+				buttonType="filled"
+				size="lg"
+				rounded={false}
+				block={false}
+				iconOnly={false}
+				ripple="light"
+				onClick={() => router.push('/')}
+			>
+				Download
+				<Icon name="file_download" size="sm" />
+			</Button>
+		</div>
+	);
 };
 
 export default generate;
