5eb5bd266af3a2b38ea9d6664c21c50436bf38f0
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2021-08-28 17:23:52 -0400

    added flip card functionality (not working)

--- client/pages/result.js
+++ client/pages/result.js
@@ -1,57 +1,77 @@
-import Button from '@material-tailwind/react/Button';
-import Icon from '@material-tailwind/react/Icon';
-import { useRouter } from 'next/router';
-import Input from '@material-tailwind/react/Input';
-import { useState, useEffect } from 'react';
-import Image from 'next/image';
-import { saveAs } from 'file-saver';
-import Swal from 'sweetalert2';
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+import { useRouter } from "next/router";
+import Input from "@material-tailwind/react/Input";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { saveAs } from "file-saver";
+import Swal from "sweetalert2";
+import ReactCardFlip from "react-card-flip";
 
 const generate = () => {
   const router = useRouter();
-  const [memeUrl, setMemeUrl] = useState();
+  const [unansweredMemeUrl, setUnansweredMemeUrl] = useState();
+  const [answeredMemeUrl, setAnsweredMemeUrl] = useState();
+  const [flipped, setFlipped] = useState();
+  const handleClick = () => {
+    setFlipped(!flipped);
+  };
   useEffect(() => {
-    setMemeUrl(window.sessionStorage.getItem('meme'));
+    setUnansweredMemeUrl(window.sessionStorage.getItem("unanswered_meme"));
+    setAnsweredMemeUrl(window.sessionStorage.getItem("answered_meme"));
   }, []);
   const downloadMeme = async () => {
     try {
-      if (memeUrl) {
-        await saveAs(memeUrl, 'image.jpg');
-        Swal.fire('Success', 'Downloading meme...', 'success').then(() =>
-          router.push('/')
+      if (answeredMemeUrl) {
+        await saveAs(answeredMemeUrl, "image.jpg");
+        Swal.fire("Success", "Downloading meme...", "success").then(() =>
+          router.push("/")
         );
       } else
-        Swal.fire('Error', 'Resource not available', 'error').then(() =>
-          router.push('/')
+        Swal.fire("Error", "Resource not available", "error").then(() =>
+          router.push("/")
         );
     } catch {
-      Swal.fire('Error', 'Resource not available', 'error').then(() =>
-        router.push('/')
+      Swal.fire("Error", "Resource not available", "error").then(() =>
+        router.push("/")
       );
     }
   };
   return (
-    <div className='bg-[#FFE501] overflow-hidden min-w-screen min-h-screen flex justify-center items-center flex-col gap-16'>
-      {memeUrl ? (
-        <div className='bg-white p-5 rounded-lg'>
-          <Image src={memeUrl} width={600} height={500} />
+    <div className="bg-[#FFE501] overflow-hidden min-w-screen min-h-screen flex justify-center items-center flex-col gap-16">
+      {answeredMemeUrl ? (
+        <div className="bg-white p-5 rounded-lg">
+          <ReactCardFlip isFlipped={flipped} flipDirection="vertical">
+            <Image
+              src={answeredMemeUrl}
+              width={600}
+              height={500}
+              onClick={handleClick}
+            />
+            <Image
+              src={unansweredMemeUrl}
+              width={600}
+              height={500}
+              onClick={handleClick}
+            />
+          </ReactCardFlip>
         </div>
       ) : (
-        <p className='text-5xl text-purple-600'>Sorry, you have no memes</p>
+        <p className="text-5xl text-purple-600">Sorry, you have no memes</p>
       )}
       <Button
-        className='group'
-        color='deepPurple'
-        buttonType='filled'
-        size='lg'
+        className="group"
+        color="deepPurple"
+        buttonType="filled"
+        size="lg"
         rounded={false}
         block={false}
         iconOnly={false}
-        ripple='light'
+        ripple="light"
         onClick={downloadMeme}
       >
         Download
-        <Icon name='file_download' size='sm' />
+        <Icon name="file_download" size="sm" />
       </Button>
     </div>
   );
